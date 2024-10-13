00c15c695ec93e09dd8d02ea7690bef3a588bb1f
Denkishi <denkishikurasame@gmail.com>
2024-10-13 11:31:55 +0200

    Update start.py

--- .github/python/start.py
+++ .github/python/start.py
@@ -18,8 +18,12 @@ while i<proj_num:
 	title=data["projects"][i]["title"]
 	project_token=data["projects"][i]["token"]
 	latest_run=data["projects"][i]["last_ready_run"]["run_token"]
-	run_token_string="https://www.parsehub.com/api/v2/runs/"+latest_run
-	requests.delete(run_token_string, params=params)
-	project_token_string="https://www.parsehub.com/api/v2/projects/"+project_token+"/run"
-	requests.post(project_token_string, data=params)
+	if latest_run is None:
+		project_token_string="https://www.parsehub.com/api/v2/projects/"+project_token+"/run"
+		requests.post(project_token_string, data=params)
+	if latest_run is not None:
+		run_token_string="https://www.parsehub.com/api/v2/runs/"+latest_run
+		requests.delete(run_token_string, params=params)
+		project_token_string="https://www.parsehub.com/api/v2/projects/"+project_token+"/run"
+		requests.post(project_token_string, data=params)
 	i+=1
