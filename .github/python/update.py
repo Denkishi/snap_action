fb286e966ff3758f14500da94b84b5d36cf1bda9
Denkishi <denkishikurasame@gmail.com>
2024-11-20 20:25:01 +0100

    Update update.py

--- .github/python/update.py
+++ .github/python/update.py
@@ -4,15 +4,18 @@ import os
 
 token = os.environ["TOKEN"]
 params = {
-  "api_key": token,
-  "offset": "0",
-  "limit": "20",
-  "include_options": "1"
+    "api_key": token,
+    "offset": "0",
+    "limit": "20",
+    "include_options": "1"
 }
+
 r = requests.get('https://www.parsehub.com/api/v2/projects', params=params)
-data=json.loads(r.text)
-#print(data["total_projects"])
-proj_num=(data["total_projects"]) #numero progetti
+data = json.loads(r.text)
+
+# Numero totale di progetti
+proj_num = data["total_projects"]
+
 i = 0
 while i < proj_num:
     title = data["projects"][i]["title"]
@@ -29,10 +32,14 @@ while i < proj_num:
         j = requests.get(run_token_string, params=params)
         json_file = json.loads(j.text)
         
-        # Salva il file JSON
-        file_name = title + ".json"
-        with open(file_name, 'w', encoding='utf-8') as f:
-            json.dump(json_file, f, ensure_ascii=False, indent=4)
+        # Controlla se il contenuto del JSON non è vuoto o "{}"
+        if json_file and json_file != {}:
+            file_name = title + ".json"
+            
+            # Salva il file JSON
+            with open(file_name, 'w', encoding='utf-8') as f:
+                json.dump(json_file, f, ensure_ascii=False, indent=4)
+        else:
+            print(f"Il contenuto del JSON per '{title}' è vuoto. File non salvato.")
     
     i += 1
-
